29a5d8cdfa5e402d7cbf2fc176235b97f0783b45
vrushabhjv <vrushabhjv2@gmail.com>
2024-12-07 00:02:19 +0530

    minor changes

--- tasks/models.py
+++ tasks/models.py
@@ -3,19 +3,11 @@ from django.contrib.auth.models import User
 from datetime import datetime, timedelta
 import os
 
-
-def next_day_morning():
-    """
-    Returns the next day's morning (8:00 AM) as a datetime object.
-    """
-    now = datetime.now()
-    next_day = now + timedelta(days=1)
-    return next_day.replace(hour=8, minute=0, second=0, microsecond=0)
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    reminder_schedule = models.DateTimeField(default=next_day_morning())
+    reminder_schedule = models.DateTimeField()
     attachment = models.FileField(upload_to='attachments/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
